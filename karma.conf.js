9a3f08c4034bcbf6bfe8682a46f219eec228c74a
Tiago A. de Oliveira <tiagoo@ciandt.com>
2020-02-27 14:15:55 -0300

    application name adjustments

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/web-miniapp'),
+      dir: require('path').join(__dirname, './coverage/web-miniapp-fake'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
